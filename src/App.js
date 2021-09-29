6059f2dec664c9ec39fc6c47c2d2e3e6e79e1bac
ahmetbirsen <87116175+ahmetbirsen@users.noreply.github.com>
2021-09-29 16:53:38 +0300

    Yazi listesi oluşturuldu , yazi detayi olusturuldu , apiden yazılar çekildi

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router,Route } from "react-router-dom";
+import YaziListesi from "./components/YaziListesi";
+import YaziDetayi from "./components/YaziDetayi";
+
+function App() {
+  return (
+    <Router>
+      <div className="main-wrapper">
+        <header></header>
+        <div className="ui raised very padded text container segment">
+          <Route exact path="/" component = {YaziListesi}/>
+          <Route path="/posts/:id" component={YaziDetayi}/>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/YaziListesi.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const YaziListesi = (props) => {
+  const [yaziListesi, setYaziListesi] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://react-yazi-yorum.herokuapp.com/posts")
+      .then((response) => {
+        setYaziListesi(response.data);
+      });
+  }, []);
+
+  return (
+    <div className="ui relaxed divided list">
+      {yaziListesi.map((yazi) => {
+        return (
+          
+            <div className="item" key={yazi.id}>
+              <i className="large github middle aligned icon"></i>
+              <div className="content">
+                <Link to={`/posts/${yazi.id}`} className="header" href="/">
+                  {yazi.title}
+                </Link>
+                <div className="description">{yazi.created_at}</div>
+              </div>
+            </div>
+          
+        );
+      })}{" "}
+    </div>
+  );
+};
+export default YaziListesi;
